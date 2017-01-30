ad09b7c4ac940d670875bc75d81b76fd733cd52e
alsbtain <info@alsbtain.net>
2017-01-29 23:50:48 -0800

    Update

--- www/settings.js
+++ www/settings.js
@@ -14,6 +14,6 @@ var externalMenu = {
 
 var copyRight = 'www.abdulhai.net';
 
-var shareFooter = "\\n\\nمن تطبيق حسينية أبي الفضل العباس بالتركية:\\n\\nللأندرويد:\\nhttp://sbta.in/al3bbas_Android\\n\\nلنظام iOS:\\nhttp://sbta.in/al3bbas_iOS";
+var shareFooter = "\\n\\nمن تطبيق الخطيب الحسيني عبدالحي آل قمبر:\\n\\nللأندرويد:\\nhttp://sbta.in/abdulhai_Android\\n\\nلنظام iOS:\\nhttp://sbta.in/abdulhai_iOS";
 
 var AndroidSenderID = "548569982355";
